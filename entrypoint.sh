2702c5dfb7b571d228308256884c0c4f774c832c
Aviad Reich <aviad.r@team8.vc>
2021-08-24 16:58:40 +0300

    feat: OX-9 policy 6

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,6 @@ cd /                      # Github's actions/checkout@v2 is `cd`ing
                           # into /github/workspace (and deletes its
                           # contents)
 
-python -m shhhhh.policies $1 $2
+echo "$GITHUB_EVENT_PATH"
+cat "$GITHUB_EVENT_PATH"
+python -m shhhhh.policies "$1" "$2"
